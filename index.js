7c98b5eec821975b5d75babb75b8ea504603e6f1
Ammar Shah <ammarshah123@gmail.com>
2018-03-24 15:43:43 +0500

    Update reply message

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function handleEvent(event) {
   const echo = { type: 'text', text: event.message.text };
 
   // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  return client.replyMessage(event.replyToken, "Bot says: " + echo);
 }
 
 // listen on port
